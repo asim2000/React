00dc2e4cc7d55e2222500c43fddd70db3efa0daa
asimbabayev <asimbabayev0002@gmail.com>
2023-06-12 00:29:39 +0400

    Added react-router-dom

--- intro/src/ProductList.js
+++ intro/src/ProductList.js
@@ -1,63 +1,44 @@
-import React, { Component } from 'react'
-import { Button, Table } from 'reactstrap'
+import React, { Component } from "react";
+import { Button, Table } from "reactstrap";
 
 export default class ProductList extends Component {
-    
   render() {
     return (
       <div>
-        <Table
->
-  <thead>
-    <tr>
-      <th>
-        Id
-      </th>
-      <th>
-        Product Name
-      </th>
-      <th>
-      QuantityPerUnit
-      </th>
-      <th>
-      UnitPrice
-      </th>
-      <th>
-      UnitsInStock
-      </th>
-      <th>
-        Action
-      </th>
-    </tr>
-  </thead>
-  <tbody>
-    {
-        this.props.products.map(product => {
-           return <tr key={product.id}>
-      <th scope="row">
-        {product.id}
-      </th>
-      <td>
-        {product.productName}
-      </td>
-      <td>
-        {product.quantityPerUnit}
-      </td>
-      <td>
-        {product.unitPrice}
-      </td>
-      <td>
-        {product.unitsInStock}
-      </td>
-      <td>
-        <Button color='info' onClick={()=>this.props.addToCart(product)}>Add</Button>
-      </td>
-    </tr>
-        }) 
-    }
-  </tbody>
-</Table>
+        <Table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Product Name</th>
+              <th>QuantityPerUnit</th>
+              <th>UnitPrice</th>
+              <th>UnitsInStock</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.products.map((product) => {
+              return (
+                <tr key={product.id}>
+                  <th scope="row">{product.id}</th>
+                  <td>{product.productName}</td>
+                  <td>{product.quantityPerUnit}</td>
+                  <td>{product.unitPrice}</td>
+                  <td>{product.unitsInStock}</td>
+                  <td>
+                    <Button
+                      color="info"
+                      onClick={() => this.props.addToCart(product)}
+                    >
+                      Add
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
       </div>
-    )
+    );
   }
 }
